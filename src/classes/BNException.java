db329a25d79f7c7156a93a1c59efafabf2694708
ivser96 <zis96@ukr.net>
2015-06-07 22:40:58 +0300

    Catch MyException
    Catch the Exception about wrong choice of sign variants

--- src/classes/BNException.java
+++
@@ -1,7 +0,0 @@
-package classes;
-
-/**
- * Created by Общий on 07.06.2015.
- */
-
-
--- src/classes/MyException.java
+++ src/classes/MyException.java
@@ -3,7 +3,7 @@ package classes;
 /**
  * Created by Общий on 07.06.2015.
  */
-class MyException extends Exception {
+public class MyException extends Exception {
     private int a;
     public String toString(){ return "Input action number:"+ a + ">2"; }
 }
